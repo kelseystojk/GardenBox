d1cccefbd8922d1ebae33759e6f7e6e2ac7cc7ea
kelseystojk <78608283+kelseystojk@users.noreply.github.com>
2022-06-08 12:02:35 -0400

    Registering garden box enabled, currently following the template but will modify to serve a real purpose.

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -2,7 +2,9 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class HomeController {
@@ -27,5 +29,11 @@ public class HomeController {
         //model.addAttribute("listProfession", listProfession);
 
         return "register_form";
+    }
+
+    @PostMapping("/register")
+    public String submitForm(@ModelAttribute("gardenBox") GardenBox gardenBox) {
+        System.out.println(gardenBox);
+        return "register_success";
     }
 }
